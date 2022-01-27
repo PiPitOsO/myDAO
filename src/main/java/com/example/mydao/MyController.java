bb6b1707149437aae2d90326b106c3f98c19e841
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2022-01-27 05:06:38 +0300

    2

--- src/main/java/com/example/mydao/MyController.java
+++ src/main/java/com/example/mydao/MyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MyController {
     @GetMapping()
     public String name() {
-        return "hiiii";
+        return "gi";
     }
 
     @GetMapping("/products/fetch-product")
