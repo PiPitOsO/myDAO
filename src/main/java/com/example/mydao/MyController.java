82f69720fa2af6a7c2f7c31354609ced9e5e1c0f
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2022-01-30 16:00:10 +0300

    4

--- src/main/java/com/example/mydao/MyController.java
+++ src/main/java/com/example/mydao/MyController.java
@@ -2,17 +2,10 @@ package com.example.mydao;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
-//    @GetMapping()
-//    public String name() {
-//        return "gi";
-//    }
-
-//    @GetMapping("/products/fetch-product")
     @GetMapping()
     public String name(@RequestParam(required = false) String name) {
         System.out.println("hi " + name);
