23a283f7eab18f4e9970d05af372f949eac989ab
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2022-01-30 15:23:42 +0300

    2

--- src/main/java/com/example/mydao/MyController.java
+++ src/main/java/com/example/mydao/MyController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping()
 public class MyController {
     @GetMapping()
     public String name() {
